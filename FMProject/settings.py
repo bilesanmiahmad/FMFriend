467732e6a86abde3ccc9889ac66d1f5edf4fb0d9
Bilesanmi Ahmad <sandraoamriagyeiwaa@gmail.com>
2018-05-11 17:45:40 +0000

    deployment settings

--- FMProject/settings.py
+++ FMProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'FMProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fmassistant',
+        'USER': 'ahmad',
+        'PASSWORD': 'signout87',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
@@ -119,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
